86d44e8bad9eceee7db782b96f869395ff5f013b
Naveen M K <naveen@syrusdark.website>
2020-10-07 11:15:23 +0530

    Try wheel

--- setup.py
+++ setup.py
@@ -6,9 +6,18 @@ if sys.version_info.major < 3:
     raise RuntimeError(
         'cairocffi does not support Python 2.x anymore. '
         'Please use Python 3 or install an older version of cairocffi.')
+from wheel.bdist_wheel import bdist_wheel
 
+class BdistWheel(bdist_wheel):
+    def get_tag(self):
+        return ('py3', 'none') + bdist_wheel.get_tag(self)[2:]
+cmdclass = {
+    'bdist_wheel': BdistWheel,
+}
 setup(
+    cmdclass=cmdclass,
     cffi_modules=[
         'cairocffi/ffi_build.py:ffi',
-        'cairocffi/ffi_build.py:ffi_pixbuf']
+        'cairocffi/ffi_build.py:ffi_pixbuf'],
+    setup_requires=['setuptools_scm[toml]']
 )
